716bde0bfe23f2aab2a9b5228ecdd5453f98c97a
Norm Santos <santnorm@gmail.com>
2023-05-26 11:31:49 -0600

    Add more files.

---
+++ index.mjs
@@ -0,0 +1,36 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { to } from 'await-to-js';
+import _ from 'lodash';
+import axios from "axios";
+import { mapSeries } from 'modern-async';
+const { Client } = require('pg');
+
+
+async function run() {
+	const client = new Client({
+		connectionString: core.getInput('connectionString'),
+	});
+	await client.connect()
+
+	const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+	console.log(res.rows[0].message) // Hello world!
+	await client.end()
+
+
+	core.info(`Waiting ${res.rows[0].message} milliseconds ...`);
+/*
+	core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+	await wait(parseInt(ms));
+	core.info((new Date()).toTimeString());
+
+	core.setOutput('time', new Date().toTimeString());
+*/
+}
+
+
+try {
+	run();
+} catch (error) {
+	core.setFailed(error.message);
+}
---
+++ test.sh
@@ -0,0 +1,4 @@
+export INPUT_PREFECT_ACCOUNT_KEY="pcu_Aw3MCMauILpYkW66p2yMPFJfOG3dq626mv8x"
+export INPUT_ACTION="stop"
+export INPUT_BRANCH="branch4"
+node index.mjs
