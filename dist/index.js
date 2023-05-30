a5fc2bddbf747498dc64bc203488341f2e6c710f
Norm Santos <santnorm@gmail.com>
2023-05-30 17:55:21 -0600

    Ensure the correct build is there.

--- dist/index.js
+++ dist/index.js
@@ -32975,9 +32975,11 @@ async function dropType(name, c) {
 async function deleteUser(email, c) {
   core.info(`Delete User : ${email}`);
   const u = await c.query(`SELECT email FROM "auth"."users" WHERE email = '${email}';`);
-  if (u.length > 0) {
-    return c.query(`DELETE FROM "auth"."users" WHERE email = "${email}";`);
+  if (u.rows.length > 0) {
+    core.info(`Found User : ${email} deleting...`);
+    return c.query(`DELETE FROM "auth"."users" WHERE email = '${email}';`);
   }
+  core.info(`Delete User: ${email} not found, skipping...`);
   return true;
 }
 async function run() {
