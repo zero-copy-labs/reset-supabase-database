c566e197c0ac27155774a3b13101764486efe82d
Norm Santos <santnorm@gmail.com>
2023-07-16 16:56:59 -0600

    Remove Materialized Views as well.

--- dist/index.js
+++ dist/index.js
@@ -15181,6 +15181,20 @@ var tablesAndViews = `
 SELECT table_name, table_type FROM information_schema.tables 
 WHERE table_schema = 'public';
 `;
+var materializedViews = `select schemaname as schema_name,
+       matviewname as view_name,
+       matviewowner as owner,
+       ispopulated as is_populated,
+       definition
+from pg_matviews
+where schemaname = 'public'
+order by schema_name,
+         view_name;
+`;
+async function dropMaterializedView(name, c) {
+  core2.info(`Drop Materialized View: ${name}`);
+  return c.query(`DROP MATERIALIZED VIEW IF EXISTS "${name}" CASCADE;`);
+}
 async function dropView(name, c) {
   core2.info(`Drop View: ${name}`);
   return c.query(`DROP VIEW IF EXISTS "${name}" CASCADE;`);
@@ -15190,6 +15204,10 @@ async function dropTable(name, c) {
   return c.query(`DROP TABLE IF EXISTS "${name}" CASCADE;`);
 }
 async function run2(c) {
+  const { rows: mViews } = await c.query(materializedViews);
+  await forEachSeries_default(mViews, async (view) => {
+    return dropMaterializedView(view.view_name, c);
+  });
   const { rows: tables } = await c.query(tablesAndViews);
   await forEachSeries_default(tables, async (table) => {
     if (table.table_type === "VIEW") {
--- src/tables-and-views.mjs
+++ src/tables-and-views.mjs
@@ -4,7 +4,25 @@ import { forEachSeries } from 'modern-async';
 const tablesAndViews = `
 SELECT table_name, table_type FROM information_schema.tables 
 WHERE table_schema = 'public';
-`
+`;
+
+const materializedViews = `select schemaname as schema_name,
+       matviewname as view_name,
+       matviewowner as owner,
+       ispopulated as is_populated,
+       definition
+from pg_matviews
+where schemaname = 'public'
+order by schema_name,
+         view_name;
+`;
+
+async function dropMaterializedView(name, c) {
+	core.info(`Drop Materialized View: ${name}`);
+
+	return c.query(`DROP MATERIALIZED VIEW IF EXISTS "${name}" CASCADE;`)
+}
+
 async function dropView(name, c) {
 	core.info(`Drop View: ${name}`);
 
@@ -18,6 +36,13 @@ async function dropTable(name, c) {
 }
 
 export default async function run(c) {
+	// Find all materialized views in the public schema
+	const { rows: mViews }= await c.query(materializedViews)
+
+	await forEachSeries(mViews, async (view) => {
+		return dropMaterializedView(view.view_name, c);
+	})
+
 	// Find all tables and views in the public schema
 	const { rows: tables }= await c.query(tablesAndViews)
 
